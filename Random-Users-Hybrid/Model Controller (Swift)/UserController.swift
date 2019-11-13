dce222faa7379c9d1ec90514fe527277d4a80aa8
Dongwoo Pae <dongwoopae@gmail.com>
2019-11-13 21:50:36 +0900

    implemented DWPUser model

--- Random-Users-Hybrid/Model Controller (Swift)/UserController.swift
+++ Random-Users-Hybrid/Model Controller (Swift)/UserController.swift
@@ -1,17 +1,9 @@
 //
-//  User.h
+//  UserController.swift
 //  Random-Users-Hybrid
 //
 //  Created by Dongwoo Pae on 11/13/19.
 //  Copyright © 2019 Dongwoo Pae. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface User : NSObject
-
-@end
-
-NS_ASSUME_NONNULL_END
+import Foundation
---
+++ Random-Users-Hybrid/Model/DWPUser.h
@@ -0,0 +1,23 @@
+//
+//  User.h
+//  Random-Users-Hybrid
+//
+//  Created by Dongwoo Pae on 11/13/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DWPUser: NSObject
+
+@property (nonatomic, copy, readonly, nonnull) NSString *firstName;
+@property (nonatomic, copy, readonly, nonnull) NSString *email;
+@property (nonatomic, copy, readonly, nonnull) NSString *phoneNumber;
+@property (nonatomic, copy, readonly, nonnull) NSString *imageURLString;
+
+-(nonnull instancetype)initWithFirstName:(nonnull NSString *)firstName email:(nonnull NSString *)email phoneNumber:(nonnull NSString *)phoneNumber imageURLString:(nonnull NSString *)imageURLString;
+
+-(nullable instancetype)initWithDictionary:(nonnull NSDictionary *)dictionary;
+
+@end
+
